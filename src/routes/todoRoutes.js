b2d00452b01f45b38e99acc20d36f711c23f1a70
Nesa Saravana Kumar <zanalex010@gmail.com>
2026-02-28 15:25:39 +0000

    todoroutes

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -5,7 +5,11 @@ const router = express.Router()
 
 //get all todos for the logged in user
 
-router.get('/', (req,res) => {})
+router.get('/', (req,res) => {
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userID)
+    res.json(todos)
+})
 
 //create a new todo
 router.post('/', (req,res) => {})
